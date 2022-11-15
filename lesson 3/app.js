bb3d7dd8bf157b5ecbfbfdf596472764215ef3aa
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-15 23:29:22 +0500

    if else ,  etrnary operator

--- lesson 3/app.js
+++ lesson 3/app.js
@@ -1,8 +1,22 @@
 
+// if else
+
+
 let time = 20;
-if(time > 0 && time <= 12) console.log("Xayrli tong");
-else if(time > 12 && time <= 18)  console.log("Xayrli kun");
-else     console.log("Xayrli tun");
+if(time > 0 && time <= 12){
+
+     console.log("Xayrli tong");
+}else if(time > 12 && time <= 18){
+     console.log("Xayrli kun");
+
+}  
+else  {
+     console.log("Xayrli tun");
+}   
+
+
+// ternary operator
+
 
 time > 0 && time <= 12 ? console.log("Xayrli tong") : time > 12 && time <= 18 ? console.log("Xayrli kun") : console.log("Xayrli tun");
 
