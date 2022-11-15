47f34068e9a7c53801a83de6f5ae1aa4e067d265
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-15 23:24:05 +0500

    if else ,  etrnary operator

--- lesson 3/app.js
+++ lesson 3/app.js
@@ -1,3 +1,8 @@
-// start javascript 
 
-console.log('Hello javascript');
+let time = 20;
+if(time > 0 && time <= 12) console.log("Xayrli tong");
+else if(time > 12 && time <= 18)  console.log("Xayrli kun");
+else     console.log("Xayrli tun");
+
+time > 0 && time <= 12 ? console.log("Xayrli tong") : time > 12 && time <= 18 ? console.log("Xayrli kun") : console.log("Xayrli tun");
+
